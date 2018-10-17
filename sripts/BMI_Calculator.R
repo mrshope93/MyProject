dd944308647310b9f5b00ef81be8006b2de49a45
mrshope93 <nadezda.fursova@bioch.ox.ac.uk>
2018-10-17 23:58:05 +0100

    Update BMI_Calculator.R

--- sripts/BMI_Calculator.R
+++ sripts/BMI_Calculator.R
@@ -1,13 +1,5 @@
 #######Function to calculate a BMI
 
-####Input your weight in kg
-
-weight <- 60
-
-###Input your height in m
-
-height <- 1.7
-
 ####Calculate Body Mass Index (BMI)
 
 BMI <- function(weight, height) {
